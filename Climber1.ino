4a820f1e6ace905e5e5b55fd9588f92697f164e5
Adam Bryant <bryant.adam@gmail.com>
2013-03-21 17:05:24 -0600

    All software interrupts working as planned

--- Climber1.ino
+++ Climber1.ino
@@ -1,21 +1,47 @@
 //#include <Arduino.h>
 #include <PinChangeInt.h>
+#include <FlexiTimer2.h>
+#include <Servo.h>
 
 #define TOP_LIMIT     5
 #define BOTTOM_LIMIT  6
 #define LED_OUT       13
 #define LED2_OUT      12
+#define LED3_OUT      11
+#define BODY_SERVO    8
+#define SHUTTLE_MOTOR 9
 
 volatile int top_state = LOW;
 volatile int bottom_state = LOW;
 
+unsigned char desiredAngle = 30;
+volatile unsigned char servoAngle = 90;
+char servoDir = 0;
+
 uint8_t latest_interrupted_pin;
 uint8_t interrupt_count[20]={0}; // 20 possible arduino pins
+
+Servo bodyServo;
+Servo shuttleMotor;
 
 void quicfunc() 
 {
   latest_interrupted_pin=PCintPort::arduinoPin;
   interrupt_count[latest_interrupted_pin]++;
+}
+
+void tmr2Int()
+{
+//  static boolean output = HIGH;
+//  digitalWrite(LED3_OUT,output);
+//  output = !output;
+  servoAngle += servoDir;
+}
+
+void limitSwitchInt()
+{
+  top_state = !top_state;
+  digitalWrite( LED2_OUT, top_state );
 }
 
 void setup() 
@@ -25,6 +51,7 @@ void setup()
   pinMode( BOTTOM_LIMIT, INPUT );
   pinMode( LED_OUT, OUTPUT );
   pinMode( LED2_OUT, OUTPUT );
+  pinMode( LED3_OUT, OUTPUT );
 
   // Turn on internal pullups  
   digitalWrite(TOP_LIMIT, HIGH);
@@ -34,6 +61,12 @@ void setup()
   PCintPort::attachInterrupt(BOTTOM_LIMIT, &quicfunc, FALLING);
   
   attachInterrupt( 1, limitSwitchInt, RISING );
+  
+  FlexiTimer2::set(20,tmr2Int);
+  FlexiTimer2::start();
+  
+  bodyServo.attach(BODY_SERVO);
+  shuttleMotor.attach(SHUTTLE_MOTOR);
 }
 
 void loop() 
@@ -43,10 +76,35 @@ void loop()
     digitalWrite(LED_OUT, LOW);
   else if (latest_interrupted_pin == BOTTOM_LIMIT)
     digitalWrite(LED_OUT, HIGH);
+    
+  /*
+  bodyServo.write(30);
+  delay(2000);
+  bodyServo.write(150);
+  delay(2000);
+  */
+  if (servoAngle > desiredAngle)
+    servoDir = -1;
+  else if (servoAngle < desiredAngle)
+    servoDir = 1;
+  else
+    servoDir = 0;
+    
+  
+  if (servoAngle == desiredAngle)
+  {
+    if (desiredAngle == 30)
+    {
+      desiredAngle = 150;
+      shuttleMotor.write(95);
+    }
+    else
+    {
+      desiredAngle = 30;
+      shuttleMotor.write(75);
+    }
+  }  
+  bodyServo.write(servoAngle);
 }
 
-void limitSwitchInt()
-{
-  top_state = !top_state;
-  digitalWrite( LED2_OUT, top_state );
-}
+
